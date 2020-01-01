aa094b7cc58b9628db6ab91a8f7f2e2101762d92
Centaurus99 <1102569568@qq.com>
2020-01-01 14:05:58 +0800

    代码重构

--- run.py
+++ run.py
@@ -20,11 +20,12 @@ getheader={
 
 cur = -1
 Timeout = 5
+Left = 0
 def paint(x,y,col):
     global cur
     cur=(cur+1)%len(cookies)
     
-    while (time.time() - last_time[cur]) < 10 :
+    while (time.time() - last_time[cur]) < 10.5 :
         time.sleep(0.5)
     
     data={
@@ -37,22 +38,31 @@ def paint(x,y,col):
         "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
         "cookie":cookies[cur]
     }
-    response = requests.post("https://www.luogu.com.cn/paintBoard/paint",data=data,headers=headers)
-    status = response.json()['status']
+    try:
+        response = requests.post("https://www.luogu.com.cn/paintBoard/paint",data=data,headers=headers)
+        status = response.json()['status']
+    except:
+        status = -1
+    
+    global Left
     if status == 200:
-            print('[200] Success by', cur, '! (', x , y , col , ')')
-            last_time[cur] = time.time()
-            return 0
+        Left -= 1
+        print('[200] Success by', cur, '! (', x, y, col,') |' , Left, 'need to do')
+        last_time[cur] = time.time()
+        return 0
     elif status == 401:
             print('[401] Not login.')
     elif status == 500:
             print('[500] Please wait for trying again.')
     else:
-            print('[???] Unknown error.')
+            print('[???] Unknown error.', '[', status, ']')
     return -1
 
 while True:
-    getboard=requests.get("https://www.luogu.com.cn/paintBoard/board",headers=getheader)
+    try:
+        getboard=requests.get("https://www.luogu.com.cn/paintBoard/board",headers=getheader)
+    except:
+        continue
     todo = []
     for i in board:
         x=i[0]
@@ -60,18 +70,15 @@ while True:
         col=i[2]
         if x*401+y<len(getboard.text) and int(getboard.text[x*401+y],32)!=col:
             todo.append(i)
-    cnt = len(todo)
-    # print(todo)
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , "[Info]:Have", cnt , 'to do')
-    if cnt == 0:
+    Left = len(todo)
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) , "[Info]:Have", Left , 'to do')
+    if Left == 0:
         time.sleep(Timeout)
-    for i in range(cnt):
+    for i in range(Left):
         bd = todo[i]
         x=bd[0]
         y=bd[1]
         col=bd[2]
         while True:
-            # if cur==0:
-            #     getboard=requests.get("https://www.luogu.com.cn/paintBoard/board",headers=getheader)
             if paint(x,y,col)==0:
                 break
